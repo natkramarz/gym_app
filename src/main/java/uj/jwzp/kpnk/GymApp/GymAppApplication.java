e45ddd0a97bda7ffa29b4f59d1593ecfa5e75646
Katarzyna Płoskonka <kasplosk@gmail.com>
2022-02-25 16:36:45 +0100

    Initial commit

---
+++ src/main/java/uj/jwzp/kpnk/GymApp/GymAppApplication.java
@@ -0,0 +1,13 @@
+package uj.jwzp.kpnk.GymApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uj/jwzp/kpnk/GymApp/GymAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uj.jwzp.kpnk.GymApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
