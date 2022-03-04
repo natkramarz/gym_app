7bb29e546758fd6998a5320b2008653997a4cdf1
Katarzyna Płoskonka <kasplosk@gmail.com>
2022-03-04 16:52:11 +0100

    Fix typo

--- release-docker-in-heroku.sh
+++ release-docker-in-heroku.sh
@@ -5,15 +5,15 @@
 # $2 - Heroku API key
 
 echo "Releasing Heroku application with name $1."
-IMAGE_ID=$(docker inspect registry.heroku.com/$1/web --format{{.Id}})
+IMAGE_ID=$(docker inspect registry.heroku.com/$1/web --format={{.Id}})
 echo "Image id: $IMAGE_ID"
 PAYLOAD='{"updates":[{"type":"web","docker_image":"'"$IMAGE_ID"'"}]}'
 echo "Payload: $PAYLOAD"
 
 curl \
--n \
--X PATCH https://api.heroku.com/apps/$1/formation \
--d "$PAYLOAD" \
--H "Content-Type: application/json" \
--H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
--H "Authorization: Bearer $2"
+ -n \
+ -X PATCH https://api.heroku.com/apps/$1/formation \
+ -d "$PAYLOAD" \
+ -H "Content-Type: application/json" \
+ -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+ -H "Authorization: Bearer $2"
